9875d99eb2dce7036a7846610b562cab190e033b
CurorVult <41748740+CurorVult@users.noreply.github.com>
2022-12-24 04:21:20 +0000

    Added refference

--- Capture.py
+++ Capture.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 # Set Camera Parameters
-#Code adapted from computervision.zone's 3 Hour Computer Vision workshop
-widthImg =640
-heightImg =480
+#Code adapted in part from computervision.zone's 3 Hour Computer Vision workshop  https://www.computervision.zone/courses/learn-opencv-in-3-hours/
+widthImg =780
+heightImg =1280
 capture= cv2.VideoCapture(1)
 capture.set(3,widthImg)
 capture.set(4, heightImg)
@@ -33,7 +33,7 @@ def getContours(img):
         area = cv2.contourArea(cnt)
         print(area)
         if area>5000:
-            #cv2.drawContours(imgContour,cnt,-1,(255,0,0), 3)
+            #Determine the shape of each contour
             peri = cv2.arcLength(cnt,True)
             aprx = cv2.approxPolyDP(cnt,0.02*peri,True)
             #Here we find the contour with the largest area by looping over every bound area with 4 sides in the frame
@@ -42,22 +42,41 @@ def getContours(img):
                 maxArea=area
     #cv2.drawContours(imgContour, largest,-1,(255,0,0), 20)        
     return largest
+
+
 def reorder(points):
     #This function reorders the array of points to ensure that proper bounding boxes are created
-    points= points.reshape((4,3))
-    pass
+    points= points.reshape((4,2))
+    newPoints=np.zeros((4,1,2),np.int32)
+    #sum points along first axis
+    add = points.sum(1)
+    #assign smallest point as the top left corner and the largest as bottom right.
+    newPoints[0]=points[np.argmin(add)]
+    newPoints[3]=points[np.argmax(add)]
+    #assign the difference between the two middle points as the bottom left and top right
+    diff=np.diff(points,axis=1)
+    newPoints[1]=points[np.argmin(diff)]
+    newPoints[2]=points[np.argmax(diff)]
+    return newPoints
+
+    
 
 
 
 
 def getWarp(img,largest):
 #define two sets of points 
+    reorder(largest)
     pts1=np.float32(largest)
     pts2= np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    #Perspective warp the area within the bounding box of the largest image captured
     matrix = cv2.getPerspectiveTransform(pts1,pts2)
     imgOut= cv2.warpPerspective(img, matrix, (widthImg,heightImg))
-
-    return imgOut
+    #crop warp corrected image, removing 10 pixels from each side
+    imgCrop=imgOut[10:imgOut.shape[0]-10,10:imgOut.shape[1]-10]
+    #resize image to same size once more
+    imgCrop= cv2.resize(imgCrop,(widthImg,heightImg))
+    return imgCrop
 
 
 
@@ -70,10 +89,14 @@ while True:
     #apply filters
     imgContour= img.copy()
     imgThresh=imageProcessing(img)
+    #find the largest 4 sided "object" in the camera field of view and draw a bounding box around it
     largest = getContours(imgThresh)
+    #Correct for warped/skewed perception
     getWarp(img,largest)
 
     cv2.imshow("Result",imgThresh)
+    #here will go code to take still image
+
     #Quit feed, will remove in final result
     if cv2.waitKey(1) & OxFF==ord('q'):
         break
